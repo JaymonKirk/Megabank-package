4f064e840017fed1372e1233941067a763bdcac3
JaymonKirk <92777604+JaymonKirk@users.noreply.github.com>
2021-11-05 15:54:07 +0800

    Update t_test.R

--- R/t_test.R
+++ R/t_test.R
@@ -21,11 +21,18 @@ bar_graph <- ggplot(data = megabank_data, aes(x= netball_skills, fill = departme
     x = "Netball skills",
     y = "Count",
     title = "Netball Skills by Department",
-    caption = "There is an 83.4% chance that a person drawn at random from Human Resources
+    caption = "
+    Human Resources mean = 3.73, Customer Service mean = 2.33
+
+    t(58) = 5.31, p < .05
+
+    Cohen's d = 1.37
+
+   There is an 83.4% chance that a person drawn at random from Human Resources
     will be better at netball than a person drawn at random from Customer Service.") +
   theme(
     plot.title = element_text(size = 16, hjust = 0.5),
-    plot.caption = element_text(size = 12, hjust = 0.5))+
+    plot.caption = element_text(size = 12, hjust = 0.5)) +
   scale_fill_discrete(
     name = "Department",
     labels = c("Customer Service","Human Resources")
@@ -33,11 +40,7 @@ bar_graph <- ggplot(data = megabank_data, aes(x= netball_skills, fill = departme
 
 effect_size <- cohen.d(megabank_data$netball_skills[megabank_data$department == "hr"],megabank_data$netball_skills[megabank_data$department == "cs"])
 
-print(hr_mean)
-print(cs_mean)
-print(t_test_output)
 plot(bar_graph)
-print(effect_size)
 }
 
 #Something to take the R output from the t-test and to make it pretty
